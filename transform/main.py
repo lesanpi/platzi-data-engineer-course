f86381592c215c27bfabb5c21ee7dddee72accb8
Luis Sanchez <lespinerua@gmail.com>
2021-03-18 21:08:30 -0400

    2. ETL

--- transform/main.py
+++ transform/main.py
@@ -4,6 +4,8 @@ logging.basicConfig(level=logging.INFO)
 from urllib.parse import urlparse
 import pandas as pd
 import hashlib
+import nltk
+from nltk.corpus import stopwords
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +19,11 @@ def main(filename):
     df = _fill_missing_titles(df)
     df = _generate_uids_for_rows(df)
     df = _remove_new_lines_from_body(df)
+    df = _tokenize_column(df, 'title')
+    df = _tokenize_column(df, 'body')
+    df = _remove_duplicate_entries(df, 'title')
+    df = _drop_rows_with_missing_values(df)
+    _save_data(df, filename)
 
     return df
 
@@ -46,10 +53,11 @@ def _fill_missing_titles(df: pd.DataFrame):
     logger.info("Filling missing titles")
     missing_titles_mask = df['title'].isna()
 
-    missing_titles = (df[missing_titles_mask]['url'].str.extract('r(?P<missing_titles>[^/]+)$')
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
                         .applymap(lambda title: title.split('-'))
-                        .applymap(lambda title: ' '.join(title))
-                        )
+                        .applymap(lambda title_word_list: ' '.join(title_word_list))
+                     )
 
     df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
     return df
@@ -57,7 +65,7 @@ def _fill_missing_titles(df: pd.DataFrame):
 def _generate_uids_for_rows(df: pd.DataFrame):
     logger.info("Generating uids for each row")
     uids = (df
-            .apply(lambda row: hashlib.hashlib.md5(bytes(row['url'].encode())), axis = 1)
+            .apply(lambda row: hashlib.md5(bytes(row['url'].encode())), axis = 1)
             .apply(lambda hash_obj: hash_obj.hexdigest())
     )
     df['uid'] = uids
@@ -71,6 +79,40 @@ def _remove_new_lines_from_body(df: pd.DataFrame):
     df['body'] = stripped_body
     return df
 
+def _tokenize_column(df, column_name):
+    logger.info('Calculating the number of unique tokens in {}'.format(column_name))
+    stop_words = set(stopwords.words('spanish'))
+
+    n_tokens =  (df
+                 .dropna()
+                 .apply(lambda row: nltk.word_tokenize(row[column_name]), axis=1)
+                 .apply(lambda tokens: list(filter(lambda token: token.isalpha(), tokens)))
+                 .apply(lambda tokens: list(map(lambda token: token.lower(), tokens)))
+                 .apply(lambda word_list: list(filter(lambda word: word not in stop_words, word_list)))
+                 .apply(lambda valid_word_list: len(valid_word_list))
+            )
+
+    df['n_tokens_' + column_name] = n_tokens
+
+    return df
+
+def _remove_duplicate_entries(df, column_name):
+    logger.info('Removing duplicate entries')
+    df.drop_duplicates(subset=[column_name], keep='first', inplace=True)
+
+    return df
+
+
+def _drop_rows_with_missing_values(df):
+    logger.info('Dropping rows with missing values')
+    return df.dropna()
+
+
+def _save_data(df, filename):
+    clean_filename = 'clean_{}'.format(filename)
+    logger.info('Saving data at location: {}'.format(clean_filename))
+    df.to_csv(clean_filename)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('filename',
@@ -79,4 +121,3 @@ if __name__ == "__main__":
     arg = parser.parse_args()
     df = main(arg.filename)
 
-    print(df)
